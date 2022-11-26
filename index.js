bfcf9aada10cf97e9a7b51f0631e6df0dd93b7bf
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-11-26 07:49:19 +0530

    Exercise done

--- index.js
+++ index.js
@@ -308,6 +308,79 @@ for(let key in c1){ // return all instance and prototype properties
 }
  // we can check property is own property or not using - c1.hasOwnProperty('radius') => output true
 
+ //Avoid Extending the Built-in Objects
+ //Don't modify built in objects
+
+ function Stopwatch1(){
+    let startTime, endTime, running, duration = 0;
+
+    Object.defineProperty(this,'duration',{
+        get: function(){
+            return duration;
+        },
+        set : function(value){
+            duration = value;
+        }
+    });
+
+    Object.defineProperty(this,'startTime',{
+        get: function(){
+            return startTime;
+        }
+    });
+
+    Object.defineProperty(this,'endTime',{
+        get: function(){
+            return endTime;
+        }
+    });
+
+    Object.defineProperty(this,'running',{
+        get: function(){
+            return running;
+        }
+    });
+}
+
+Stopwatch1.prototype.start = function(){ 
+
+    if (this.running){
+        throw Error('Stopwatch is already running');
+    }
+    else{
+        running = true;
+    }
+
+    this.startTime = new Date();
+}
+
+Stopwatch1.prototype.stop = function(){
+
+    if(!this.running){
+        throw Error('Stopwatch is not start running');
+    }
+    else{
+        this.running = false;
+    }
+
+    this.endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime())/1000;
+    duration +=seconds;
+
+}
+
+
+Stopwatch1.prototype.reset = function(){
+    this.startTime = null;
+    this.endTime = null;
+    this.running = false;
+    this.duration = 0;
+}
+
+const sw1 = new Stopwatch1();
+sw1.duration = 20;
+
 
 
 

15e28a8288defa9ae9ebcc68248668ff5328af49
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-11-26 11:06:31 +0530

    Intermediate Function Inheritance

--- prototypical_inheritance.js
+++ prototypical_inheritance.js
@@ -2,20 +2,29 @@ function Shape(color){
 this.color = color;
 }
 
+function extend(Childr,Parent) //parameters starting with capital letter, because of these are connect with constructor functions
+{
+    Childr.prototype = Object.create(Parent.prototype);
+    Childr.prototype.constructor = Childr;
+}
+
 function Circle (radius,color){
     Shape.call(this,color);
     this.radius = radius;
 }
 
+extend(Circle,Shape);
+
 Circle.prototype.draw = function(){
     console.log('draw');
 }
 
-//Circle.prototype = Object.create(Object.prototype); before creat shape and , this is default
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
+//Circle.prototype = Object.create(Object.prototype); before creat shape and , this is default(no need to below 2 lines because of line 5)
 
-Circle.prototype.duplicate = function(){
+//Circle.prototype = Object.create(Shape.prototype);
+//Circle.prototype.constructor = Circle;
+
+Shape.prototype.duplicate = function(){
     console.log('duplicate');
 }
 
@@ -30,5 +39,26 @@ const c = new Circle(2,'red');
 //Circle.prototype.constructor() => similar => new Circle(); - creating new object
 
 //Calling the Super Constructor - line 6
+
+// Intermediate Function Inheritance
+
+//imagine we need to add another constructor class called, Square.
+
+function Square(size){
+    this.size = size;
+}
+
+extend(Square,Shape);
+
+const sq = new Square(10);
+
+//we need to add prototype reset and constructor reser after this function
+
+//Square.prototype = Object.create(Shape.prototype);
+//Square.prototype.constructor = Square;
+
+//using these 2 lines over and over is a mess. to avoid this, we can use new function to make this re-usable (line 5)
+
+
 
 

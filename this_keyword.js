779ea57761a1b20803aaf5e716d6167fa8948d84
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-11-30 15:27:25 +0530

    Private Members Using Symbols

--- this_keyword.js
+++ this_keyword.js
@@ -26,4 +26,24 @@ const pr = new Piremed();
 pr.draw();
 console.log(pr.draw);
 
-draw();//output undefined, whether strict mode or not, because of method inside class declaration behave like strict mode. (don't give output as windows.object)
+//draw();//output undefined, whether strict mode or not, because of method inside class declaration behave like strict mode. (don't give output as windows.object)
+
+//Private Members Using Symbols
+
+const _radius = Symbol();//gives unique radius value, every times that Symbol() calling
+const _draw = Symbol();
+
+class Circle1{
+    constructor(radius){
+        this[_radius] = radius;
+    }
+
+    [_draw](){
+
+    }
+
+}
+
+const c2=new Circle1(2);
+const key = Object.getOwnPropertySymbols(c2)[0];
+console.log(c2[key]);

cbd3f3c32c73f18eda1d024aa473dec4a38ac5f3
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-01 00:16:47 +0530

    Solution for stack exercise

--- this_keyword.js
+++ this_keyword.js
@@ -136,3 +136,42 @@ class Bag extends Color{
 const bg =  new Bag();
 console.log(bg.paint());//output method call in the Bag class.if you want to call parent class's paint method, you need to add super.paint(); to the child class's method
 
+//Solution for stack exercise
+
+const _items = new WeakMap();
+
+class Stack{
+    constructor(){
+        _items.set(this,[]);//add empty array in weakmap
+    }
+
+    push(obj){
+        
+        _items.get(this).push(obj);
+    }
+
+    pop(){
+        const items = _items.get(this);
+
+        if(items.length ===0)
+         throw new Error('Stack is empty');
+        
+         return items.pop();
+    }
+
+    peek(){
+        const items = _items.get(this);
+
+        if(items.length ===0)
+         throw new Error('Stack is empty');
+
+        return items[items.length-1];
+    }
+
+    get count(){
+        return _items.get(this).length;
+    }
+}
+
+const stk = new Stack();
+
